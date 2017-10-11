8d807ae56c87edb9a651bdd8846921e246cfec2f
admin <dangtinhtong@gmail.com>
2017-10-11 23:49:38 +0700

    First commit

--- Fractions/main.m
+++ Fractions/main.m
@@ -11,7 +11,11 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+       // NSLog(@"Hello, World!");
+        // Simple propram to work with fractions
+        int  numerator =1;
+        int denominator=3;
+        NSLog(@"The fraction is %i/%i", numerator, denominator);
     }
     return 0;
 }
