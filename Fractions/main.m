5ca117cdd12a5b470c4ae332018d282e291a3783
admin <dangtinhtong@gmail.com>
2017-10-12 00:43:57 +0700

    - Show to work with more than one fraction in program.

--- Fractions/main.m
+++ Fractions/main.m
@@ -26,6 +26,9 @@
 // Program to work with fractions -- Use class version
 
 // Interface section
+
+// The @interface section desribes the class, its data components, and its methods
+/*
 @interface Fraction : NSObject
 {
     int numerator;
@@ -39,6 +42,7 @@
 
 
 // Implementation section
+// The @implemetation section contains the actual code that implements thes method
 @implementation Fraction
 
 
@@ -59,6 +63,8 @@
 @end
 
 // Program section
+
+// The program section contains the program code to carry out the intended purpose of the program
 
 int main(int argc, const char * argv[]) {
     
@@ -81,4 +87,71 @@ int main(int argc, const char * argv[]) {
     
   //  [m//yFraction release];
     //
+}*/
+
+//*********************************************************
+// Set one fraction to 2/3 and set another to 3/7 and display them both
+// Program to work with fraction
+
+// interface section
+// interface section
+
+// The @interface section describles the class, its data components and its methods
+@interface Fraction : NSObject
+{
+    int numeration;
+    int denominator;
+    
+}
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+
+@end
+
+//
+// implementation section
+// The @implementation section contains the actual code that implements these methods
+@implementation Fraction
+// print
+-(void) print{
+    NSLog(@"%i/%i", numeration, denominator);
+}
+// setNumerator
+-(void) setNumerator:(int)n{
+    numeration = n;
+}
+
+// setDenominator
+-(void) setDenominator:(int)d   {
+    denominator = d;
+}
+@end
+
+
+// Program section
+// The program section contains the program code to carry out the intended purpose of the program
+int main(int argc, const char * argv[]) {
+
+    Fraction *myFraction1 =[[Fraction alloc]init];
+    Fraction *myFraction2 =[[Fraction alloc]init];
+//Set 1st fraction
+    [myFraction1 setNumerator:2];
+    [myFraction1 setDenominator:4];
+    // set 2nd fraction
+    
+    [myFraction2 setNumerator:5];
+    [myFraction2 setDenominator:8];
+    
+    // Display the fractions
+    NSLog(@"The first fraction is:");
+    [myFraction1 print];
+    
+    
+    NSLog(@"The second fraction is:");
+    [myFraction2 print];
+    
+    
+    
 }
